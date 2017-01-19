561a09403015a988e0699cabaa975fe5f7741005
johnmu <johnmu@umnhoj.com>
2017-01-18 22:28:38 -0800

    start simple file reading

--- gol-golang/gol.go
+++ gol-golang/gol.go
@@ -1,8 +1,43 @@
 package main
 
-
-import {"os"
-"fmt"}
+import (
+	"os"
+	"log"
+	"fmt"
+	"bufio"
+)
 
 func main() {
+	args := os.Args[1:]
+	if len(args) != 3{
+		log.Println("Incorrect number of arguments, please see ../README.md for details")
+		os.Exit(1)
+	}
+
+	//numGenerations := args[0]
+	inputGrid := args[1]
+	//outputGif := args[2]
+
+	// Parse the RLE file
+	{
+		rleFile, err := os.Open(inputGrid)
+		if err != nil {
+			fmt.Printf("error opening file: %v\n",err)
+			os.Exit(1)
+		}
+
+		rleScanner := bufio.NewScanner(rleFile)
+
+		for rleScanner.Scan() {
+			fmt.Println(rleScanner.Text())
+		}
+
+		if err := rleScanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Run iterations
+
+	// output the GIF
 }
